3eeeb07af1bf86ecfa7cde62fe7b0732c147b04a
vuquocdat <youremail@gmail.com>
2025-09-20 00:08:13 +0700

    ko

---
+++ buoi7/b1$b2b7.js
@@ -0,0 +1,8 @@
+let a = 2, b = 3;
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+console.log("Phần dư:", a % b);
+console.log("a mũ b:",a**b);
